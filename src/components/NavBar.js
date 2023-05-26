09ff844966a68890d9fec35a6cdf39194d504764
Brian Ashman <brian.ashman@comcast.net>
2023-05-26 11:48:25 -0700

    Fixed NavBar <a> attributes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function NavBar() {
   return <nav>
-    <a href="home">Home</a>
-    <a href="about">About</a>  
+    <a href="#home">Home</a>
+    <a href="#about">About</a>  
   </nav>;
 }
 
